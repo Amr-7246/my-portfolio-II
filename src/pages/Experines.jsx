a0650cd2b1469153da090260182d02b4a7625025
Amr Ehab <aehab7246@gmail.com>
2025-07-28 13:25:50 +0300

    Done

--- src/pages/Experines.jsx
+++ src/pages/Experines.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Experience, Tech } from '../components'
+import LayerMask from '../components/LayerMask'
 
 const Experines = () => {
 return (
     <div className='pt-[100px]'>
+        <LayerMask/>
         <Experience />
         <Tech />
     </div>
